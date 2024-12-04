d98ff6e204bfcc625d01874ec7de1637b1565d96
Khalil L <mortyl0834@gmail.com>
2024-12-04 17:19:51 +0800

    ros1 neotic ubuntu2004 supprt

--- start_docker.sh
+++ start_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --runtime=nvidia --name vins_backend --gpus all --net=host  -v /home/dji/workspace/VINS-Fusion:/root/catkin_ws/src/VINS-Fusion   ros:vins-fusion    /bin/bash
+docker run -it --runtime=nvidia --name vins_gtsam --gpus all --net=host  -v /home/dji/workspace/VINS-Backend:/root/catkin_ws/src/VINS-Fusion ros:vins-fusion    /bin/bash
